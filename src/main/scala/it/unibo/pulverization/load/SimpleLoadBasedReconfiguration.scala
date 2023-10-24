ec5982c4612aa23084eea18dcd06120dd12a80a8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-10-24 11:21:55 +0200

    feat: first stub for baseline scenario

--- src/main/scala/it/unibo/pulverization/load/SimpleLoadBasedReconfiguration.scala
+++ src/main/scala/it/unibo/pulverization/load/SimpleLoadBasedReconfiguration.scala
@@ -10,5 +10,46 @@ class SimpleLoadBasedReconfiguration
     with BlockG
     with BlockC
     with CustomSpawn {
-  override def main(): Any = ???
+  /**
+   * The baseline representing a "static" scenario in which each device has only a one device to offload the computation.
+   * The device in which offload the computation is based on the closest device.
+   */
+  override def main(): Any = {
+    val isThickHost = node.getOrElse[Boolean]("isThickHost", false)
+    val computationalCost = node.getOrElse[Double]("computationCost", 0.0)
+    val load = node.getOrElse[Double]("load", 0.0)
+
+    val potential = classicGradient(isThickHost)
+    val leaderId = G[ID](isThickHost, mid(), identity, nbrRange _)
+
+    val devicesWantOffloading =
+      C[Double, Set[(ID, Double)]](potential, _ ++ _, Set((mid(), computationalCost)), Set.empty)
+
+    var accumulator = load
+    val devicesCanOffloading = devicesWantOffloading.toList
+      .sortBy(_._2)
+      .takeWhile { case (_, cost) =>
+        val condition = accumulator + cost <= 100.0
+        accumulator = accumulator + cost
+        condition
+      }
+      .toSet
+
+    node.put("[DEBUG] devicesCanOffloading", devicesCanOffloading)
+    node.put("[DEBUG] devicesWantOffloading", devicesWantOffloading)
+    node.put("[DEBUG] potential", potential)
+
+    val offloadingCost = devicesCanOffloading.toList.map(_._2).sum
+    node.put("effectiveLoad", load + offloadingCost)
+
+    val devicesCanOffload = G[Set[ID]](isThickHost, devicesCanOffloading.map(_._1), identity, nbrRange _)
+    val canOffload = devicesCanOffload.contains(mid())
+
+    node.put("wantToOffload", !isThickHost)
+    node.put("canOffload", canOffload)
+
+    node.put("leaderID", leaderId)
+    // node.put("leaderID", if (canOffload) leaderId else -1)
+    node.put("leaderEffect", leaderId % 10)
+  }
 }
