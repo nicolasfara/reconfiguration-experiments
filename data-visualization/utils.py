ceec5d43ba09e650da8dbd7b56d68160f19c55b8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-10-31 16:59:14 +0100

    chore: add utils package

---
+++ data-visualization/utils.py
@@ -0,0 +1,181 @@
+import numpy as np
+import xarray as xr
+import re
+from pathlib import Path
+import collections
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cmx
+import matplotlib.font_manager as fm
+
+def distance(val, ref):
+    return abs(ref - val)
+vectDistance = np.vectorize(distance)
+
+def cmap_xmap(function, cmap):
+    """ Applies function, on the indices of colormap cmap. Beware, function
+    should map the [0, 1] segment to itself, or you are in for surprises.
+
+    See also cmap_xmap.
+    """
+    cdict = cmap._segmentdata
+    function_to_map = lambda x : (function(x[0]), x[1], x[2])
+    for key in ('red','green','blue'):
+        cdict[key] = map(function_to_map, cdict[key])
+#        cdict[key].sort()
+#        assert (cdict[key][0]<0 or cdict[key][-1]>1), "Resulting indices extend out of the [0, 1] segment."
+    return matplotlib.colors.LinearSegmentedColormap('colormap',cdict,1024)
+
+def getClosest(sortedMatrix, column, val):
+    while len(sortedMatrix) > 3:
+        half = int(len(sortedMatrix) / 2)
+        sortedMatrix = sortedMatrix[-half - 1:] if sortedMatrix[half, column] < val else sortedMatrix[: half + 1]
+    if len(sortedMatrix) == 1:
+        result = sortedMatrix[0].copy()
+        result[column] = val
+        return result
+    else:
+        safecopy = sortedMatrix.copy()
+        safecopy[:, column] = vectDistance(safecopy[:, column], val)
+        minidx = np.argmin(safecopy[:, column])
+        safecopy = safecopy[minidx, :].A1
+        safecopy[column] = val
+        return safecopy
+
+def convert(column, samples, matrix):
+    return np.matrix([getClosest(matrix, column, t) for t in samples])
+
+def valueOrEmptySet(k, d):
+    return (d[k] if isinstance(d[k], set) else {d[k]}) if k in d else set()
+
+def mergeDicts(d1, d2):
+    """
+    Creates a new dictionary whose keys are the union of the keys of two
+    dictionaries, and whose values are the union of values.
+
+    Parameters
+    ----------
+    d1: dict
+        dictionary whose values are sets
+    d2: dict
+        dictionary whose values are sets
+
+    Returns
+    -------
+    dict
+        A dict whose keys are the union of the keys of two dictionaries,
+    and whose values are the union of values
+
+    """
+    res = {}
+    for k in d1.keys() | d2.keys():
+        res[k] = valueOrEmptySet(k, d1) | valueOrEmptySet(k, d2)
+    return res
+
+def extractCoordinates(filename, excludes = []):
+    """
+    Scans the header of an Alchemist file in search of the variables.
+
+    Parameters
+    ----------
+    filename : str
+        path to the target file
+    mergewith : dict
+        a dictionary whose dimensions will be merged with the returned one
+
+    Returns
+    -------
+    dict
+        A dictionary whose keys are strings (coordinate name) and values are
+        lists (set of variable values)
+
+    """
+    import re
+    with open(filename, 'r') as file:
+#        regex = re.compile(' (?P<varName>[a-zA-Z._-]+) = (?P<varValue>[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?),?')
+        regex = r"(?P<varName>[a-zA-Z._-]+) = (?P<varValue>\[[\d\s,]+\]|\d+(?:\.\d+)?)"
+        dataBegin = r"\d"
+        is_float = r"[-+]?\d*\.?\d+(?:[eE][-+]?\d+)?"
+        for line in file:
+            match = re.findall(regex, line.replace('Infinity', '1e30000'))
+            if match:
+                return {
+                    var : float(value) if re.match(is_float, value)
+                        else bool(re.match(r".*?true.*?", value.lower())) if re.match(r".*?(true|false).*?", value.lower())
+                        else value
+                    for var, value in match if var not in excludes
+                }
+            elif re.match(dataBegin, line[0]):
+                return {}
+
+def extractVariableNames(filename):
+    """
+    Gets the variable names from the Alchemist data files header.
+
+    Parameters
+    ----------
+    filename : str
+        path to the target file
+
+    Returns
+    -------
+    list of list
+        A matrix with the values of the csv file
+
+    """
+    with open(filename, 'r') as file:
+        dataBegin = re.compile('\d')
+        lastHeaderLine = ''
+        for line in file:
+            if dataBegin.match(line[0]):
+                break
+            else:
+                lastHeaderLine = line
+        if lastHeaderLine:
+            regex = re.compile(' (?P<varName>\S+)')
+            return regex.findall(lastHeaderLine)
+        return []
+
+def openCsv(path):
+    """
+    Converts an Alchemist export file into a list of lists representing the matrix of values.
+
+    Parameters
+    ----------
+    path : str
+        path to the target file
+
+    Returns
+    -------
+    list of list
+        A matrix with the values of the csv file
+
+    """
+    regex = re.compile('\d')
+    with open(path, 'r') as file:
+        lines = filter(lambda x: regex.match(x[0]), file.readlines())
+        return [[float(x) for x in line.split()] for line in lines]
+
+def beautifyValue(v):
+    """
+    Converts an object to a better version for printing, in particular:
+        - if the object converts to float, then its float value is used
+        - if the object can be rounded to int, then the int value is preferred
+
+    Parameters
+    ----------
+    v : object
+        the object to try to beautify
+
+    Returns
+    -------
+    object or float or int
+        the beautified value
+    """
+    try:
+        v = float(v)
+        if v.is_integer():
+            return int(v)
+        return v
+    except:
+        return v
