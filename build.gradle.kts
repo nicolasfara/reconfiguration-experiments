97e5ca9f9079ba844b530e3b8202c8f86d6d4d45
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-11-14 21:57:37 +0100

    build: remove shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
     alias(libs.plugins.taskTree) // Helps debugging dependencies among gradle tasks
     alias(libs.plugins.kotlin)
     alias(libs.plugins.kotlinx.serialization)
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.bmuschko.docker-java-application") version "6.7.0"
 }
 
